54c02b05235ac7052995c754a8fbaabdf91cfe46
otamm <oivatom@gmail.com>
2015-03-22 01:14:00 -0300

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -5,6 +5,8 @@
 //  Created by Gerald on 2/11/14.
 //  Copyright (c) 2014 Apportable. All rights reserved.
 //
+// Node = Single unit in an hierarchy. A node hierarchy tracks which nodes influence other nodes. In an analogy, the Earth is a node, a tree is a node and a fruit is a node. What happens to the Earth directly affects the tree, which will directly affect the fruit (but not vice-versa). Tree is a child node of Earth and a parent node of fruit. Fruit is a child node of tree and earth. A parent influences the child, a child does not influence its parent.
+// In the Cocos2d framework, all the children of the physicsNode will follow the basic laws of physics (their programmed representations, in this case). That's because the physics engine used by Cocos2d (called Chipmunk) iterates through the children of the physicsNode every frame and apply physics to them (including colision and gravity, for example).
 
 #import "CCNode.h"
 #import "Character.h"
@@ -18,6 +20,10 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
 @interface GamePlayScene : CCNode <CCPhysicsCollisionDelegate>
 {
     // define variables here;
+    
+    //  \/\/\/\/ instance of the bird
+    Character*     character;  // character node; both char and physics nodes reference pre-defined nodes.
+    CCPhysicsNode* physicsNode; // physics node; both are also referenced in the GamePlayScene (main class), which makes them available in the whole game.
 }
 
 -(void) initialize;
--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,17 @@
 - (void)initialize
 {
     // your code here
+    
+    // loads a character from the 'Character' CCB file.
+    // CCB files are created by SpriteBuilder and describe appearance and properties of in-game objects like the Flappy Bird character.
+    character = (Character*)[CCBReader load:@"Character"]; // initializes the character
+    
+    
+    [physicsNode addChild:character]; // adds character as a child of physicsNode, which apply physics to it
+    
+    // call a method in Obj-C: [ObjectName methodName];
+    // call a method in Obj-C with a parameter: [ObjectName methodName:parameter];
+    // note: physicsNode does not have a first uppercase letter because it is actually the CCPhysicsNode* object stored in a variable.
 }
 
 -(void)update:(CCTime)delta
@@ -15,5 +26,38 @@
 }
 
 // put new methods here
+
+// declaring methods in obj-C:
+// - (type I return)nameOfMethod: (type of first parameter) firstParameter continuationOfMethodName: (type of second parameter) secondParameter
+
+// a method that does not return anything and does not accept parameters (classic 'print Hello World'):
+/*
+ -(void) doSomething
+ {
+ NSLog(@"Hello World!");
+ }
+ */
+
+// a method that returns an int and accepts a string as parameter:
+/*
+ -(int) doSomethingWithAString: (NSString*) myString
+ {
+ //returns the length of the string times ten
+ return [myString length] * 10;
+ }
+ */
+
+// a method that returns an array and accepts multiple strings as parameters:
+/*
+ -(NSArray *) addThisStringToAnArray:(NSString*) firstString andThisString:(NSString*) secondString andAlsoThisString:(NSString*) thirdString
+ {
+ return [NSArray arrayWithObjects: firstString, secondString, thirdString, nil];
+ }
+ */
+
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap]; // whenever the screen is touched, the method 'flap' will be called on the character. This causes the character to impulse upwards (courtesy of the physicsNode)
+}
 
 @end
